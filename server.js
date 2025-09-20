4dd63e28077fdbc7205d971cce50fdf49d64239a
Coder-sai2004 <dcmeramsai003@gmail.com>
2025-09-20 13:23:18 +0530

    update 1

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-require("dotenv").config({ path: "./mongo.env" });
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -425,3 +424,4 @@ app.get('/ping', (req, res) => res.send('pong'));
 app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
 });
+
